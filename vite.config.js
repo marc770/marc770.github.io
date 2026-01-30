34902c0dfaec2a0c521e91fed643f5ad1d3fbbb8
Marcus Andersson <marcus.andersson@sr.se>
2026-01-30 13:33:04 +0100

    pwa test

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
+
+import { VitePWA } from 'vite-plugin-pwa'
+
 export default {
   build: {
     outDir: 'docs'
-  }
+  },
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true } // IMPORTANT for localhost
+    })
+  ]
+
 };
